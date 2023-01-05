13bd41faa0d80e8cd3386a138ecb29f8eef86bb1
florian <florian.vialet@gmail.com>
2023-01-05 16:56:53 +0100

    debutJS

---
+++ penalty.js
@@ -0,0 +1,15 @@
+let gardienGauche  = document.querySelector('.gardien_gauche')
+let gardienCentre  = document.querySelector('.gardien_centre')
+let gardienDroite  = document.querySelector('.gardien_droite')
+
+let ballons = document.querySelectorAll('.ballon')
+
+let ballonTire = document.querySelector('.ballon_tir')
+
+let flecheGauche = document.querySelector('.flechegauche')
+let flecheCentre = document.querySelector('.flechecentre')
+let flecheDroite = document.querySelector('.flechedroite')
+
+
+gardienGauche.style.display = 'none'
+gardienDroite.style.display = 'none'
